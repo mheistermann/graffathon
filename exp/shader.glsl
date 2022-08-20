22cd5f86d5e7ca59a87a34b6fd471a329e48dae3
Martin Heistermann <git@mheistermann.de>
2022-08-20 23:02:13 +0200

    get shifty

--- exp/shader.glsl
+++ exp/shader.glsl
@@ -31,28 +31,32 @@ float saw(float x) {
     return 2*abs(mod(x, 1)-.5);
 }
 
-float saw2(float x) {
-    float thr=.9;
-    float eps=.1;
+float saw2(float x,float speed) {
+    float thr=.5;
+    float eps=.5/speed;
     return smoothstep(thr-eps,thr+eps,2*abs(mod(x, 1)-.5));
 }
 
-float[9] intervals = float[9](3.,5.,7.,11.,13.,17,19,23,29);
+float[7] intervals = float[7](7.,11.,13.,17,19,23,29);
 
 float plop(vec2 uv, float off) {
     
     //return saw2(uv.x);
-    float n = 9.;
-    float noi = noise(rot(.01*time) * uv);
+    float n = 7;
+    //float noi = noise(rot(.01*time) * uv);
     float saw = mod(uv.x, 1.);
     float cnt = floor(uv.x);
-    float strip = mod(cnt,n);
-    float interval = intervals[int(strip+.5)];
+    float strip = mod(10*cnt,n);
+    //return strip/8;
+    float interval = intervals[int(strip)%9];
+    float shift= saw2(.79+(1*time)/interval,interval);
+    //return mod(interval/100,1);
     //mod(time, interval)
-    float shift = saw2(off+strip/n+.1*time); //aw2(4.*(uv.x+.4*time));
+    //float shift = saw2((time)/interval);//+.00001*(time+off)); //aw2(4.*(uv.x+.4*time));
+    //return shift/5;
     float bright = (strip+1)/(n+1);
     bright = 1;
-    return bright * noise(uv+vec2(0, shift/3));
+    return bright * noise(uv+vec2(0, shift));
     
 }
 
@@ -61,14 +65,15 @@ vec3 color(vec2 uv) {
     //float n = noise(rot(.01*time) * uv);
     //return vec3(plop(rot(PI/3)*uv));
     vec3 col;
+    uv*=9;
     
-    uv *= 6+sin(time);
-    uv.x += sin(.4*time);
+    //uv *= 6+sin(time);
+    //uv.x += sin(.4*time);
     
-    uv.x += sin(.51*time+.2*sin(2*time));
-    col += plop(uv,0);
-    col += plop(rot(2*PI/3)*uv,5);
-    col += plop(rot(4*PI/3)*uv,19);
+    //uv.x += sin(.51*time+.2*sin(2*time));
+    col += vec3(0,1,0)*plop(uv,0);
+    col += vec3(1,0,0)*plop(rot(2*PI/3)*uv,44.723);
+    col += vec3(0,0,1)*plop(rot(4*PI/3)*uv,3.9);
     return col;
 
 }
