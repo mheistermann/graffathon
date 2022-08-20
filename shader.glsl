bd3e67f6f085bd33f63064d9275234fae621ee5f
Martin Heistermann <git@mheistermann.de>
2022-08-20 12:34:47 +0200

    indent

--- shader.glsl
+++ shader.glsl
@@ -15,19 +15,19 @@ layout(location = 0) out vec4 out_color; // out_color must be written in order t
 
 
 vec3 col(vec2 uv) {
-	vec2 scaled_uv = uv*5;
-	vec2 pos = fract(scaled_uv);
-	vec2 cell = floor(scaled_uv);
-	
-	
-	const vec2 spoints[6] = vec2[6](
-		vec2(0,0),
-		vec2(0.5,0),
-		vec2(1,0),
-		vec2(1,.5),
-		vec2(1,1),
-		vec2(.5,.5)
-	);
+  vec2 scaled_uv = uv*5;
+  vec2 pos = fract(scaled_uv);
+  vec2 cell = floor(scaled_uv);
+
+
+  const vec2 spoints[6] = vec2[6](
+      vec2(0,0),
+      vec2(0.5,0),
+      vec2(1,0),
+      vec2(1,.5),
+      vec2(1,1),
+      vec2(.5,.5)
+      );
   const float x[6] = float[6]( 1,-1, 1, 1, 1, -1);
 
   float total = 0;
@@ -37,21 +37,21 @@ vec3 col(vec2 uv) {
     float d = dot(diff, diff);
     total += x[i] * d;
   }
-	
-	//vec3 x = vec3(1,1,0);
-	
-	
-	return vec3(.3*total);
-	//return vec3(fract(pos*5), 0);
+
+  //vec3 x = vec3(1,1,0);
+
+
+  return vec3(.3*total);
+  //return vec3(fract(pos*5), 0);
 }
 
 void main(void)
 {
-	vec2 uv = out_texcoord;
-	uv -= 0.5;
-	uv /= vec2(v2Resolution.y / v2Resolution.x, 1);
+  vec2 uv = out_texcoord;
+  uv -= 0.5;
+  uv /= vec2(v2Resolution.y / v2Resolution.x, 1);
 
-	
-	vec3 rgb = col(uv);
-	out_color = vec4(rgb, 1);
+
+  vec3 rgb = col(uv);
+  out_color = vec4(rgb, 1);
 }
